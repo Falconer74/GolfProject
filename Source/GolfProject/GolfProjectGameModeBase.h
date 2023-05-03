b0335977886639a81d852989ce1ead1e18860cd0
Artem Streltsov <artemstrelcov2001@gmail.com>
2023-05-03 14:19:04 +0300

    Init commit. - Added copyright line - Added .gitignore - Added .clang-format - Created TestLevel

---
+++ Source/GolfProject/GolfProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GolfProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLFPROJECT_API AGolfProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
